c620e3801250ed2545a17f537469390e3e9e3038
David Pikas <david@pikas.se>
2020-10-24 20:44:50 +0200

    Server can host static ui files

---
+++ ui/build/precache-manifest.ee37776644a3aa8aea2829f6ee71ec88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4c3ce92950a7cda8f7d6ee153bed74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef65fbcc662eea27658",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "38f128fe3fff9455b90c",
+    "url": "/static/css/main.dd1db4c9.chunk.css"
+  },
+  {
+    "revision": "cef65fbcc662eea27658",
+    "url": "/static/js/2.cdadbbbb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cdadbbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f128fe3fff9455b90c",
+    "url": "/static/js/main.656b6788.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);

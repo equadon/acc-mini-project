0bdb70d60202507d923cf17ae648b8435e6c3e8d
David Pikas <david@pikas.se>
2020-10-25 12:39:41 +0100

    Some files where missing in the previous commit

---
+++ ui/build/precache-manifest.73b7dedb362c06f4d457f7943fd465cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db64186cb315e179a783f501037a12b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "816af3c05589d4f61b74",
+    "url": "./static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "992209cd6b912cff1582",
+    "url": "./static/css/main.dd1db4c9.chunk.css"
+  },
+  {
+    "revision": "816af3c05589d4f61b74",
+    "url": "./static/js/2.43a5a075.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.43a5a075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992209cd6b912cff1582",
+    "url": "./static/js/main.82aba91f.chunk.js"
+  },
+  {
+    "revision": "b9792abf4a1da54ea494",
+    "url": "./static/js/runtime-main.16943de7.js"
+  }
+]);
